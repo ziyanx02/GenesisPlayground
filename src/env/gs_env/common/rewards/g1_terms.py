96f8a6f29f75acffac15ef978ed0d3edb22a6f2c
ziyanx02 <ziyanx02@gmail.com>
2025-09-21 12:10:20 -0400

    move constraint to g1_terms

--- src/env/gs_env/common/rewards/g1_terms.py
+++ src/env/gs_env/common/rewards/g1_terms.py
@@ -1,3 +1,5 @@
+import torch
+
 from .leggedrobot_terms import (
     ActionLimitPenalty,  # noqa
     ActionRatePenalty,  # noqa
@@ -10,8 +12,65 @@ from .leggedrobot_terms import (
     OrientationPenalty,  # noqa
     TorquePenalty,  # noqa  # noqa
 )
+from .reward_terms import RewardTerm
 
 
 ### ---- Reward Terms ---- ###
 class G1BaseHeightPenalty(BaseHeightPenalty):
     target_height = 0.75
+
+
+class WaistDofPenalty(RewardTerm):
+    """
+    Penalize the waist DoF position.
+
+    Args:
+        dof_pos: DoF position tensor of shape (B, D) where B is the batch size and D is the number of DoFs.
+    """
+
+    required_keys = ("dof_pos",)
+
+    def _compute(self, dof_pos: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return -torch.sum(torch.square(dof_pos[:, 12:15]), dim=-1)
+
+
+class UpperBodyDofPenalty(RewardTerm):
+    """
+    Penalize the upper body DoF position.
+
+    Args:
+        dof_pos: DoF position tensor of shape (B, D) where B is the batch size and D is the number of DoFs.
+    """
+
+    required_keys = ("dof_pos",)
+
+    def _compute(self, dof_pos: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return -torch.sum(torch.square(dof_pos[:, 15:]), dim=-1)
+
+
+class HipYawPenalty(RewardTerm):
+    """
+    Penalize the hip yaw DoF position.
+
+    Args:
+        dof_pos: DoF position tensor of shape (B, D) where B is the batch size and D is the number of DoFs.
+    """
+
+    required_keys = ("dof_pos",)
+
+    def _compute(self, dof_pos: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return -torch.sum(torch.square(dof_pos[:, [2, 8]]), dim=-1)
+
+
+class AnkleTorquePenalty(RewardTerm):
+    """
+    Penalize the ankle torque.
+
+    Args:
+        torque: Torque tensor of shape (B, D) where B is the batch size and D is the number of DoFs.
+    """
+
+    required_keys = ("torque",)
+
+    def _compute(self, torque: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return -torch.sum(torch.square(torque[:, [4, 5, 10, 11]]), dim=-1)
