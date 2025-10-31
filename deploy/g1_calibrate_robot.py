fix a calculation bug produced by auto-completion; remove regularization as it's useless now

deploy/g1_calibrate_robot.py
@@ -51,11 +51,11 @@ def torch_transform_RT_by(
     R1: torch.Tensor, T1: torch.Tensor, R2: torch.Tensor, T2: torch.Tensor
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """
-    Apply local transform (R2, T2) to pose (R1, T1)
-    R = R2 * R1
+    Apply local transform (R2, T2) in the frame of (R1, T1)
+    R = R1 * R2
     T = R1 * T2 + T1
     """
-    R_out = R2 @ R1
+    R_out = R1 @ R2
     T_out = (R1 @ T2.unsqueeze(-1)).squeeze(-1) + T1
     return R_out, T_out
 
@@ -65,10 +65,10 @@ def torch_get_RT_between(
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Get local transform from (R1, T1) to (R2, T2)
-    R = R2 * R1^t
+    R = R1^t * R2
     T = R1^t * (T2 - T1)
     """
-    R_out = R2 @ R1.transpose(-1, -2)
+    R_out = R1.transpose(-1, -2) @ R2
     T_out = (R1.transpose(-1, -2) @ (T2 - T1).unsqueeze(-1)).squeeze(-1)
     return R_out, T_out
 
@@ -326,7 +326,7 @@ def main(args: argparse.Namespace) -> None:
 
         rms_R_history.append(np.mean(rms_Rs))
         rms_T_history.append(np.mean(rms_Ts))
-        total_loss += 0.05 * torch.sum(torch.square(qpos_offset))
+        total_loss += 0.0 * torch.sum(torch.square(qpos_offset))
         total_loss.backward()
 
         opt.step()
@@ -356,8 +356,8 @@ def main(args: argparse.Namespace) -> None:
     print(f"[INFO] Robot calibration offsets saved to {save_path_robot}.")
 
     plt.figure(figsize=(6, 4))
-    plt.plot(rms_R_history, label="Rotation Error")
-    plt.plot(rms_T_history, label="Translation Error")
+    plt.plot(rms_R_history[900:], label="Rotation Error")
+    plt.plot(rms_T_history[900:], label="Translation Error")
     plt.xlabel("Iteration")
     plt.ylabel("Total Loss")
     plt.title("Calibration Loss Curve")

src/env/gs_env/common/utils/math_utils.py
@@ -483,10 +483,10 @@ def transform_RT_by(
 ) -> tuple[np.typing.NDArray[np.float32], np.typing.NDArray[np.float32]]:
     """
     Apply the offset (R2, T2) to the pose (R1, T1).
-    R = R2 * R1
+    R = R1 * R2
     T = R1 * T2 + T1
     """
-    R_out = np.array(gu.transform_quat_by_quat(R1, R2))
+    R_out = np.array(gu.transform_quat_by_quat(R2, R1))
     T_out = np.array(gu.transform_by_quat(T2, R1) + T1)
     return R_out, T_out
 
@@ -499,9 +499,9 @@ def get_RT_between(
 ) -> tuple[np.typing.NDArray[np.float32], np.typing.NDArray[np.float32]]:
     """
     Get the offset from (R1, T1) to (R2, T2).
-    R = R2 * R1^T
-    T = R1^T * (T2 - T1)
+    R = R1^t * R2
+    T = R1^t * (T2 - T1)
     """
-    R_out = np.array(gu.transform_quat_by_quat(gu.inv_quat(R1), R2))
+    R_out = np.array(gu.transform_quat_by_quat(R2, gu.inv_quat(R1)))
     T_out = np.array(gu.transform_by_quat(T2 - T1, gu.inv_quat(R1)))
     return R_out, T_out