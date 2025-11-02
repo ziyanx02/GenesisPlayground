bf19037bf8385a324e9fcbee23a393b06456d9e8
hanyangclarence <mahlerrrr76@gmail.com>
2025-11-01 20:06:10 -0400

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+cd ~/code/humanoid/GenesisPlayground
 source .venv/bin/activate
 export WANDB_API_KEY=28b3c634497c0dc6c16767729d4719b1012a94f2
 python examples/run_ppo_inhand_rotation.py --video_log_freq=None
